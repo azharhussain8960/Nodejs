13bc4a4dbf45e438941c5b6c468bdb9e4939c9a8
azhar hussain <hussainazhar8960@gmail.com>
2024-08-22 22:18:41 +0530

    add some code

--- Day4/filesystem.js
+++ Day4/filesystem.js
@@ -1,3 +1,4 @@
+const { promises } = require('dns')
 let fs = require('fs')
 
 // //to create the new file with the fs 
@@ -17,38 +18,67 @@ let fs = require('fs')
 
 //  File Creation With Asynchronous Method
 
-fs.writeFile("demo.txt","Hello What Is Your Name? ",(err)=>{
-    if(err){
-        console.log(`Error Generated of  ${err}`);
-    }else{
-        console.log(`File Created Successfully`);
-    }
-})
+// fs.writeFile("demo.txt","Hello What Is Your Name? ",(err)=>{
+//     if(err){
+//         console.log(`Error Generated of  ${err}`);
+//     }else{
+//         console.log(`File Created Successfully`);
+//     }
+// })
 
-fs.appendFile("demo.txt"," Is It Azhar Hussain?",(err)=>{
-    if(err){
-        console.log(`Some Error ${err}`);
-    }else{
-        console.log(`File Updated Successfully`);
+// fs.appendFile("demo.txt"," Is It Azhar Hussain?",(err)=>{
+//     if(err){
+//         console.log(`Some Error ${err}`);
+//     }else{
+//         console.log(`File Updated Successfully`);
         
-    }
-})
+//     }
+// })
 
-fs.readFile("demo.txt",(err,data)=>{
-         if(err){
-            console.log(`${err}`);
+// fs.readFile("demo.txt",(err,data)=>{
+//          if(err){
+//             console.log(`${err}`);
             
-         }else{
-            console.log(`Success ${data}`);
+//          }else{
+//             console.log(`Success ${data}`);
             
-         }
-}) 
-fs.unlink("demo.txt",(err)=>{
-    if(err){
-        console.log(`${err}`);
-    }else{
-        console.log(`Successfully Deeleted`);
+//          }
+// }) 
+// fs.unlink("demo.txt",(err)=>{
+//     if(err){
+//         console.log(`${err}`);
+//     }else{
+//         console.log(`Successfully Deeleted`);
         
-    }
-})
+//     }
+// })
 
+// fs.writeFileSync("demo.html","!")
+// fs.appendFileSync("demo.html","<h1>hello</h1>")
+// let res = fs.readFileSync("demo.html","utf8")
+// console.log(res);
+// fs.unlinkSync("demo.html")
+
+// fs.writeFile("demo.js","consol.log('hello')",(err)=>{
+//     console.log("success");
+// })
+// fs.appendFile("demo.js","let hello = 'azhar'",(err)=>{
+//     console.log("success");
+// })
+// fs.readFile("demo.js","utf8",(err,data)=>{
+//     console.log(data)
+// })
+
+// fs.unlink("demo.js",(err)=>{
+
+// })
+
+fs.writeFile("notes.txt","Java Script",(err)=>{
+    console.log("successfull");
+    
+})
+fs.appendFileSync("notes.txt",` A Promise Is Represent Somthing That Is Eventually Fullfilled.It Executes In Strict Mode Only`)
+
+fs.unlink("notes.txt",(err)=>{
+
+})
