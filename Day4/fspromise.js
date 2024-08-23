6064c99c070d3f98397fb0a1f91b131059e4c665
azhar hussain <hussainazhar8960@gmail.com>
2024-08-23 15:19:33 +0530

    add some code

---
+++ Day4/fspromise.js
@@ -0,0 +1,26 @@
+let fs = require("fs/promises")
+
+fs.writeFile("txt.txt","Hello I Am TxT").then(()=>{
+    console.log("File Created")
+}).catch(()=>{
+    console.log("Error")
+})
+
+fs.appendFile("txt.txt"," How Are You?")
+.then(()=>{
+    console.log("Successfully Generated");
+}).catch(()=>{
+     console.log("Error Generated");
+         
+})
+fs.readFile("txt.txt","utf-8").then((data)=>{
+    console.log(data);
+    
+}).catch((err)=>{
+    console.log("Error");
+})
+fs.unlink("txt.txt").then(()=>{
+    console.log("File Deleted");
+}).catch((err)=>{
+    console.log("Error");
+})
